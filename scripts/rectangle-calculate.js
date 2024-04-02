1ac0555d4caa3c7e01c72640dd35cc13e0ca89b9
arafatrahman41 <arafatrahman41@outlook.com>
2024-04-02 07:11:30 +0600

    add rectangle code

--- scripts/rectangle-calculate.js
+++ scripts/rectangle-calculate.js
@@ -1,3 +1,21 @@
 function calculateRectangleArea(){
-    console.log("calculate rect area");
+    //get width of rectangle
+    const rectangleWidth = document.getElementById('rectangle-width');
+    const rectangleWidthText = rectangleWidth.value;
+    const width = parseFloat(rectangleWidthText);
+    // console.log(width);
+
+    //get length of rectangle
+    const rectangleLength = document.getElementById('rectangle-length');
+    const rectangleLengthText = rectangleLength.value;
+    const length = parseFloat(rectangleLengthText);
+    // console.log(length);
+
+    //calculate rectangle area
+    const area = width * length;
+    console.log(area);
+
+    //display rectangle area
+    const rectangleArea = document.getElementById('rectangle-area');
+    rectangleArea.innerText = area;
 }
